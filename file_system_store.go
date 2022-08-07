b22ff2aa1c0ae4065541ea8b8ebfdd37189a8ee9
Riku <rikukestila@gmail.com>
2022-08-07 09:47:06 +0300

    Add League type, refactor

--- file_system_store.go
+++ file_system_store.go
@@ -9,34 +9,32 @@ type FileSystemPlayerStore struct {
 	database io.ReadWriteSeeker
 }
 
-func (f *FileSystemPlayerStore) GetLeague() []Player {
+func (f *FileSystemPlayerStore) GetLeague() League {
 	f.database.Seek(0, 0)
 	league, _ := NewLeague(f.database)
 	return league
 }
 
 func (f *FileSystemPlayerStore) GetPlayerScore(name string) int {
-	var wins int
+	player := f.GetLeague().Find(name)
 
-	for _, player := range f.GetLeague() {
-		if player.Name == name {
-			wins = player.Wins
-			break
-		}
+	if player != nil {
+		return player.Wins
 	}
-	return wins
+
+	return 0
+
 }
 
 //file_system_store.go
 func (f *FileSystemPlayerStore) RecordWin(name string) {
 	league := f.GetLeague()
+	player := league.Find(name)
 
-	for i, player := range league {
-		if player.Name == name {
-			// player here refers to a copy so the league is updated at the index
-			league[i].Wins++
-		}
+	if player != nil {
+		player.Wins++
 	}
+
 	f.database.Seek(0, 0)
-	json.NewEncoder(f.database).Encode(league)
+	json.NewEncoder(f.database).Encode(&league)
 }
