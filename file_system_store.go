751d46961aa6852c5417d65fdd59e9c43dd5607e
Riku <rikukestila@gmail.com>
2022-08-15 17:26:44 +0300

    Refactor by adding initialisePlayerDBFile function

--- file_system_store.go
+++ file_system_store.go
@@ -40,17 +40,10 @@ func (f *FileSystemPlayerStore) RecordWin(name string) {
 }
 
 func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
-	file.Seek(0, 0)
-
-	info, err := file.Stat()
+	err := initialisePlayerDBFile(file)
 
 	if err != nil {
-		return nil, fmt.Errorf("problem getting file info from file %s, %v", file.Name(), err)
-	}
-
-	if info.Size() == 0 {
-		file.Write([]byte("[]"))
-		file.Seek(0, 0)
+		return nil, fmt.Errorf("problem loading player store from file %s, %v", file.Name(), err)
 	}
 
 	league, err := NewLeague(file)
@@ -63,4 +56,21 @@ func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
 		database: json.NewEncoder(&tape{file}),
 		league:   league,
 	}, nil
+}
+
+func initialisePlayerDBFile(file *os.File) error {
+	file.Seek(0, 0)
+
+	info, err := file.Stat()
+
+	if err != nil {
+		return fmt.Errorf("problem getting file info from file %s, %v", file.Name(), err)
+	}
+
+	if info.Size() == 0 {
+		file.Write([]byte("[]"))
+		file.Seek(0, 0)
+	}
+
+	return nil
 }
